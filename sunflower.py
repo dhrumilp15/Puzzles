9881c099260406d5ab0c165638d90f073cc8fcd7
Dhrumil <dhrumilp15@gmail.com>
2019-02-14 21:00:52 -0500

    2018 CCC J4

--- sunflower.py
+++ sunflower.py
@@ -1,9 +1,4 @@
 #you're the sunflower...
-def printm(matrix):
-    for i in matrix:
-        for j in i:
-            print(j, end = " ")
-        print()
 length = int(input())
 matrix = []
 
@@ -12,21 +7,28 @@ for i in range(length):
 
 if matrix[0][0] < matrix[0][1]:
     if matrix[0][0] < matrix[1][0]:
-        printm(matrix)
+        #print("0")
+        for i in matrix:
+            for j in i:
+                print(j, end = " ")
+            print()
     else:
-        print("90 CCW")
-        rotated = zip(*matrix[::-1]) #rotate clockwise
-        for row in rotated:
-            print(*row) 
+        #print("90 CCW")
+        for i in range(length):
+            for j in range(length-1,-1,-1):
+                print(matrix[j][i], end = " ")
+            print()
+
 else:
     if matrix[0][length-1] > matrix[1][length-1]: # 180 case
-        print("180")
-        for i in range(length-1,0,-1):
-            for j in range(length-1, 0,-1):
+        #print(180)
+        for i in range(length-1,-1,-1):
+            for j in range(length-1, -1,-1):
                 print(matrix[i][j],end = " ")
             print()
     else: # 90 case
-        print("90")
-        rotated = zip(*matrix[::-1])
-        for row in rotated:
-            print(*row)  
+        #print("90")
+        for i in range(length-1,-1,-1):
+            for j in range(length):
+                print(matrix[j][i], end = " ")
+            print()
