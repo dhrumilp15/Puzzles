2586aa4f716bd61718498d207ea78a2eca03e362
Dhrumil Patel <dhrumilp15@gmail.com>
2019-02-19 15:53:55 -0500

    CCC 2017 J5

---
+++ nailedit.py
@@ -0,0 +1,16 @@
+woods = int(input())
+pieces = list(map(int,input().split(" ")))
+
+def boardlength(pieces):
+    sums = []
+    for i in range(woods):
+        for j in range(i + 1,woods):
+            sums.append(pieces[i] + pieces[j])
+    
+    nums = {i:sums.count(i) for i in sums if sums.count(i) > 1}
+    if len(nums) > 1:
+        return sums[01]
+    print(nums)
+    return max(nums, key=lambda key: nums[key])
+
+print(boardlength(pieces))
