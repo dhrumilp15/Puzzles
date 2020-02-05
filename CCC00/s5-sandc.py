82f148aec33dd79a5ad657ab4f947e57a5caf135
Dhrumil Patel <dhrumilp15@gmail.com>
2020-02-04 23:54:51 -0500

    Sheep and Coyotes

--- CCC00/s5-sandc.py
+++ CCC00/s5-sandc.py
@@ -1,32 +1,71 @@
 import sys
+
 input=sys.stdin.readline
 
-sheep = []
+# with open('s5.in', "r") as f:
+#     read = f.read().strip().split('\n')
+# dataiter = iter(read)
 
-with open('s5in.txt', "r") as f:
-    read = f.read()
-read = read.split("\n")
+# def input():
+#     return next(dataiter)
 
-pairs = int(read.pop(0))
-read = list(map(float, read))
-for line in range(pairs*2):
-    sheep.append((read[line - 1], read[line]))
-f.close()
+def finddist(startx, targetx, targety):
+    return (startx - targetx)**2 + targety**2
 
-def squaredist(x,y):
-    return ((y[1] - x[1])**2 + (y[0] - x[0])**2)
+sheepnum = int(input().strip())
 
-sheep.sort(key = lambda point: point[1])
-print("Sheep", str(sheep))
+spoints = [[] for i in range(sheepnum)]
 
-smallest = set()
+for i in range(2*sheepnum):
+    spoints[i//2].append(float(input().strip()))
 
-def smallsheep(x):
-    sheepdists = []
-    for index, sheepo in enumerate(sheep):
-        sheepdists.append((sheepo[0] - x)**2 + sheepo[1]**2)
+eat = [False] * sheepnum
+# Only consider sheep that can be eaten
+for sheep in spoints:
+    # print(f"From: {sheep}")
+    left = 0.0
+    right = 1000.0
+    for testsheep in spoints:
+        # print(f"\tconsidering: {testsheep}")
+        if testsheep != sheep and not eat[spoints.index(sheep)] and not eat[spoints.index(testsheep)]:
+            if testsheep[1] == sheep[1]: # same y-value
+                poi = (testsheep[0] + sheep[0]) / 2
+            else:
+                midpoint = [(testsheep[0] + sheep[0]) / 2, (testsheep[1] + sheep[1]) / 2]
+                slope =  float(sheep[0] - testsheep[0]) / float(testsheep[1] - sheep[1]) # -1/m for perp slope
+                # print(f"\t\tslope: {slope}")
+                
+                if slope == 0.0: # points are directly atop one another
+                    if testsheep[1] > sheep[1]:
+                        eat[spoints.index(testsheep)] = True
+                    else:
+                        eat[spoints.index(sheep)] = True
+                else:
+                    poi = -1.0 * (midpoint[1] / slope) + midpoint[0]
+                    if testsheep[0] < sheep[0]:
+                        left = max(poi, left)
+                    else:
+                        right = min(poi, right)
+            # print(left, right)
+    if left >= right:
+        # print(f"Removed: {sheep}")
+        eat[spoints.index(sheep)] = True
 
-for x in range(1000):
-    smallest.add(smallsheep(x))
-    for sheepo in smallest:
-        print("The sheep at {} might be eaten.".format(sheepo))
+# BF
+# x = 0.0
+# while not x > 1000.0:
+#     dists = []
+#     for sheep in spoints:
+#         dists.append(finddist(x, sheep[0], sheep[1]))
+    
+#     mindist = min(dists)
+#     for index, dist in enumerate(dists):
+#         if dist == mindist:
+#             minsheep = spoints[index]
+#             if not minsheep in chowtime:
+#                 chowtime.append(minsheep)
+#     x += 0.05
+for index, sheep in enumerate(eat):
+    if sheep == False:
+        toeat = spoints[index]
+        print(f"The sheep at ({toeat[0]:.2f}, {toeat[1]:.2f}) might be eaten.")
